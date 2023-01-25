1ccef4e19d286746dbaa64a6be4904f2a49dbc2c
Nastasia <nastasiahyam@gmail.com>
2023-01-25 03:24:40 +0000

    W3D1

---
+++ Jan23_Demo.rb
@@ -0,0 +1,19 @@
+
+
+
+  
+def find_name(name)
+    #group = Array.new
+    group = ["h","fs","boo"]
+    for n in group
+        if(name === n)
+            puts 'user exists'
+        else 
+            puts 'user doesnt'
+        end
+    end
+end
+
+find_name("test")
+   
+
--- demo.rb
+++ demo.rb
@@ -1,35 +1,55 @@
 #FizzBuzz Code Group Question
  
 #fizz buzz question
-
+#comment block
 =begin
-for i in (1..15)
-    if i % 5 == 0 and i % 3 == 0
-        puts "fizzbuzz"
-    elsif i % 3 == 0
-        puts "FIZZ"
-    elsif i % 5 == 0
-        puts "BUZZ" 
-    else 
-        puts i
+    for num in (1..15)
+        if num % 5 == 0 and num % 3 == 0
+            puts "FIZZBUZZ"
+        elsif num % 3 == 0
+            puts "FIZZ"
+        elsif num % 5 == 0
+            puts "BUZZ" 
+        else 
+            puts num
+        end
     end
-end
 =end
 
-#inclass example
-def check_position(position)
-    if position == "developer"
-        puts 'here is your link #111'
-    elsif position == 'student'
-        puts 'here is your link #222'
-    else
-        puts 'here is your link #333'
+    def yellowpages(value)
+        if value.length != 10
+            return false
+        else
+            chars = value.split("")             
+            $num = ""
+            for i in 0..chars.length do                    
+                case chars[i]
+                    when "A", "B", "C"
+                    $num += "2"
+                    when "D", "E", "F"
+                        $num += "3"
+                    when "G", "H", "I"
+                        $num += "4"
+                    when "J", "K", "L"
+                        $num += "5"
+                    when "M", "N", "O"
+                        $num += "6"
+                    when "P", "Q", "R", "S"
+                        $num += "7"
+                    when "T", "U", "V"
+                        $num += "8"
+                    when "W", "X", "Y", "Z"
+                        $num += "9"
+                end    
+                
+            end
+        #print final phone num      
+        puts $num
+        end
     end
-end
 
-myPosition = 'student'
+yellowpages("FISHSTICKS")
 
-check_position(myPosition)
 
 
 
