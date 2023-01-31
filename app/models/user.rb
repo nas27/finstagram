4e392679891ed804ba045ae42db75f7100a6cea9
Nastasia <nastasiahyam@gmail.com>
2023-01-31 00:20:54 +0000

    yellow pages question

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+#allows us to work with db via ActiveRecord, talk to db directly
+class User < ActiveRecord::Base
+
+end
--- demo.rb
+++ demo.rb
@@ -1,11 +1,11 @@
 #FizzBuzz Code Group Question
  
 #fizz buzz question
-#comment block
+#comment block, can also do % by 15 instead of 3 and 5
 =begin
     for num in (1..15)
         if num % 5 == 0 and num % 3 == 0
-            puts "FIZZBUZZ"
+            puts "FizzBuzz"
         elsif num % 3 == 0
             puts "FIZZ"
         elsif num % 5 == 0
@@ -16,16 +16,22 @@
     end
 =end
 
+#put it into a method that takes a range as argszs
+#TRY method .each_char do |letter|
+#add to uppercase to cater for lowercase
     def yellowpages(value)
         if value.length != 10
+            puts "sorry your symbols are the wrong length"
             return false
         else
-            chars = value.split("")             
+            value.upcase!
+            chars = value.split("")
+                      
             $num = ""
             for i in 0..chars.length do                    
                 case chars[i]
                     when "A", "B", "C"
-                    $num += "2"
+                        $num += "2"
                     when "D", "E", "F"
                         $num += "3"
                     when "G", "H", "I"
@@ -40,15 +46,14 @@
                         $num += "8"
                     when "W", "X", "Y", "Z"
                         $num += "9"
-                end    
-                
+                end             
             end
         #print final phone num      
         puts $num
         end
     end
 
-yellowpages("FISHSTICKS")
+yellowpages("fishyCAKES")
 
 
 
