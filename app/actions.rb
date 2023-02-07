43d58de8314779ec89e0f93898ad44050b9b0ec1
Nastasia <nastasiahyam@gmail.com>
2023-02-07 03:32:51 +0000

    W5D1-AddingSessionHandling

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,12 @@
+#can be called from any page (DRY)
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
-  @finstagram_posts = FinstagramPost.order(created_at: :desc)  
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
 
@@ -11,51 +18,41 @@ end
 post '/signup' do
   email      = params[:email]
   avatar_url = params[:avatar_url]
-  username = params[:username]
+  username   = params[:username]
   password   = params[:password]
 
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
- 
-  
-   # instantiate and save a NEW User
-   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-
-  # ActiveRecord manages input validations as defined in user.rb
-  #If fail, assigns errors to the user's errors attribute, and error messages to user.errors.full_messages then returns false.
-  #If passes it saves user to the database and returns true
-   if @user.save
-      "User #{username} saved"
-   else
-      erb(:signup)
-   end
- 
-   # return user obj in a readable format so that < isnt interpretted as a tag
-   #escape_html user.inspect
-
-  #else
-  #  escape_html user.errors.full_messages
-  #end
-  
-  #quick test check for user creation
-  #if username.present? && avatar.present?
-  #  User.create({username: username, avatar_url: avatar})
-  #  "USER was created (#{username})"
-  #else
-  #  "something went wrong"
-  #end
-
-  
+  if @user.save
+    redirect to('/login')
+  else
+    erb(:signup)
+  end
 end
 
-
-get '/login' do
-  erb(:login)
+#watches for get requests for login page
+#render the login.erb view
+get '/login' do 
+  erb(:login) 
 end
 
+post '/login' do
+  username = params[:username]
+  password = params[:password]
 
+  @user = User.find_by(username: username)
 
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    redirect to('/')
+    #"Success! User with id #{session[:user_id]} is logged in!"
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
 
-
-
-
-
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
